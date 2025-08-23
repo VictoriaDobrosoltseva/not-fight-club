52b7f199bf491f1d1e572fbebdc136a8723fc820
Viktoria Dobrosoltseva <dobrosolceva6@gmail.com>
2025-08-23 23:29:38 +0300

    feat: Adding page change functionality

---
+++ main.js
@@ -0,0 +1,24 @@
+const name = document.querySelector('.textInput');
+const buttonLogin = document.querySelector('.registerButton');
+const login = document.getElementById('login');
+const home = document.getElementById('home');
+const buttonFight = document.querySelector('.buttonFight');
+
+buttonLogin.addEventListener('click', function() {
+    const inputValue = name.value;
+    if (inputValue) 
+        {
+            localStorage.setItem('name', inputValue)
+            login.style.display = 'none';
+            home.style.display = 'flex';
+        }
+    else {
+        alert('Enter character name')
+    };
+  });
+
+buttonFight.addEventListener('click', function() {
+    home.style.display = 'none';
+    fight.style.display = 'flex';
+    heroName.textContent = characterName;
+});
